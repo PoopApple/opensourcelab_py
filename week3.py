8f688b93a46c72b4c0c495550512caa56acc659b
PoopApple <aryan1024sethi@gmail.com>
2025-08-08 11:31:45 +0530

    a

---
+++ week3.py
@@ -0,0 +1,13 @@
+def q1():
+    a = {1:2,3:4,5:6,7:8,9:0}
+    res = {}
+    for k,v in a.items():
+        res[v] = k
+    print("New dict:\n")
+    for k,v in res.items():
+        print(f"{k}:{v}")
+    
+def q2():
+    
+if __name__ == "__main__":
+    q1()
